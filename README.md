dfbdbd27cfbc2e692d12df0ff78397567b30167b
Seth Duncan <sethduncan@gmail.com>
2018-05-25 02:23:57 -0500

    Some more setup/functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # neo-lookup
 Near-Earth-Object Lookup
+
+# data
+[https://api.nasa.gov/api.html#NeoWS](https://api.nasa.gov/api.html#NeoWS)
--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 
-	// $.ajax({
-	// 	url: "https://api.nasa.gov/neo/rest/v1/feed?api_key=",
-	// 	success: function(result){
-	// 		console.log(result);
-	// 	}
-	// });
+
 
 });
+
+function displayNEOs (data) {
+	var NEOs = data.near_earth_objects;
+	console.log(NEOs);
+	for (date in NEOs) {
+		$('#neoTable').after('<tr id="' + date + '"><td style="padding-top:20px;"><b>' + date + '</b></td><td>Name</td><td>Hazard</td><td>Est. Diameter</td><td>Miss Distance</td></tr>');
+
+		for (neo in NEOs[date]) {
+			thisNEO = NEOs[date][neo];
+			hazard = (thisNEO.is_potentially_hazardous_asteroid) ? 'Yes' : 'No';
+			estDiameter = thisNEO.estimated_diameter.feet.estimated_diameter_min.toFixed(1) + ' - ' + thisNEO.estimated_diameter.feet.estimated_diameter_max.toFixed(1) + ' ft';
+			missDistance = addCommas(thisNEO.close_approach_data[0].miss_distance.miles) + ' mi';
+			$('#' + date).after('<tr><td></td><td><a href="' + thisNEO.nasa_jpl_url + '" target="_new">' + thisNEO.name + '</a></td><td>' + hazard + '</td><td>' + estDiameter + '</td><td>' + missDistance + '</td></tr>');
+		}
+
+	}
+}
+
+function addCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
