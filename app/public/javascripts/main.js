0a4ded3d9e803e6e358573e9e22278269dec2551
Seth Duncan <sethduncan@gmail.com>
2018-05-25 01:50:54 -0500

    Ok this is a functioning app at the moment

---
+++ app/public/javascripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// $.ajax({
+	// 	url: "https://api.nasa.gov/neo/rest/v1/feed?api_key=",
+	// 	success: function(result){
+	// 		console.log(result);
+	// 	}
+	// });
+
+});
---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NEO Lookup', api:process.env.NASA_API });
+});
+
+module.exports = router;
